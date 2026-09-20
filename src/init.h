54d0be43d2d9862709ae88a3a7e26507b6bf950c
Pilperi <jouni.dx@gmail.com>
2026-09-21 01:03:35 +0300

    Refaktoroi ISR

---
+++ src/init.h
@@ -0,0 +1,11 @@
+#ifndef INIT_H
+#define INIT_H
+
+// C-koodia varten:
+#ifndef __ASSEMBLER__
+
+// Datan init (.rodata, .bss)
+void init_data(void);
+
+#endif // C-koodi
+#endif // INIT_H
--- src/reset.h
+++
@@ -1,6 +0,0 @@
-#ifndef RESET_H
-#define RESET_H
-
-#define N_PAINALLUSTA 5
-
-#endif // RESET_H
